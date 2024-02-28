58d7d8a22ef8abcd93b9cff25f93975dd78b9fda
galinowsky <thougless@gmail.com>
2024-02-28 19:36:48 +0100

    Add cors package and implement CORS middleware

--- examples/scripts/vite-api.mjs
+++ examples/scripts/vite-api.mjs
@@ -1,8 +1,10 @@
 import { timeoutMiddleware } from './middlewares/timeout-middleware.mjs';
 import { createDataMiddleware } from './middlewares/data-middleware.mjs';
 import { forceErrorMiddleware } from './middlewares/force-error-middleware.mjs';
+import cors from 'cors';
 
 export const configureExpressApp = (app) => {
+  app.use(cors());
   app.use(forceErrorMiddleware);
   app.use(timeoutMiddleware);
   app.use('/api/data', createDataMiddleware());
--- practice/vue-app/src/App.vue
+++ practice/vue-app/src/App.vue
@@ -1,17 +1,65 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+
+const API_URL = 'http://localhost:3005/api/data/users?timeout=5000';
+
+const users = ref([]);
+const isFetching = ref(false);
+const isError = ref(false);
+let baseTimeout = 3000;
+
+const fetchUsers = async () => {
+  const controler = new AbortController();
+  const signal = controler.signal;
+  isError.value = false;
+  setTimeout(() => {
+    controler.abort();
+    baseTimeout = baseTimeout * 2;
+  }, baseTimeout);
+  try {
+    isFetching.value = true;
+    const data = await fetch(API_URL, { signal });
+    const parsedData = await data.json();
+    users.value = parsedData.users;
+  } catch (error) {
+    isError.value = true;
+  } finally {
+    isFetching.value = false;
+  }
+};
+
+onMounted(async () => {
+  await fetchUsers();
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div className="flex flex-row items-center justify-between py-4">
+      <h1 className="text-2xl font-bold">Users</h1>
+      <div className="flex flex-row items-center">
+        <p className="mr-2" v-if="isError">
+          Sorry, there seems to be connectivity issues...
+        </p>
+        <button
+          className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4"
+          @click="fetchUsers"
+          v-if="isError"
+        >
+          Try again
+        </button>
+      </div>
+    </div>
+    <ul className="space-y-2">
+      <li
+        v-for="user in users"
+        :key="user"
+        className="bg-white p-4 rounded-md border border-gray-100"
+      >
+        {{ user.name }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
