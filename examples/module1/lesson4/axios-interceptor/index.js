5c186de1a3d09e096f169885b0fbd6318b109869
galinowsky <thougless@gmail.com>
2024-03-03 22:16:07 +0100

    Add request and response logging in axios interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,13 +2,30 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  console.log(config)
   return config;
 });
 
+function logCurrentDateTime() {
+  const now = new Date();
+  const formattedDateTime = now.toLocaleString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+  console.log(formattedDateTime);
+}
+
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
+axios.interceptors.response.use(function (response, config) {
   // Do something with response data
-  return response;
+  console.log(config)
+    logCurrentDateTime()
+    return response;
+
 });
 
 const {
