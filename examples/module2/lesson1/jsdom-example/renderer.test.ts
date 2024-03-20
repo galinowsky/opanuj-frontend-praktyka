542b738b2e069ea65c11443b6245c58b78992770
galinowsky <thougless@gmail.com>
2024-03-20 20:28:18 +0100

    Refactor user renderer and add tests

--- examples/module2/lesson1/jsdom-example/renderer.test.ts
+++ examples/module2/lesson1/jsdom-example/renderer.test.ts
@@ -11,10 +11,10 @@ const users: User[] = [
 
 describe('User renderer', () => {
   test('should render all users if admin is rendering the list', () => {
-    localStorage.setItem('userRole', 'admin');
+    localStorage.setItem('userRole', 'admin'); // setting the user role to admin
 
-    const container = document.createElement('div');
-    renderItems(container, users);
+    const container = document.createElement('div'); // creating a container
+    renderItems(container, users); // using renderItems to render
     expect(Array.from(container.querySelectorAll('li'))).toHaveLength(3);
   });
 
@@ -24,5 +24,42 @@ describe('User renderer', () => {
     const container = document.createElement('div');
     renderItems(container, users);
     expect(Array.from(container.querySelectorAll('li'))).toHaveLength(2);
+    //test if content of the items is proper
+    
+
   });
+
+  test('should render proper content for given data with admin permissions', ()=> {
+    localStorage.setItem('userRole', 'admin'); // setting the user role to admin
+
+    const container = document.createElement('div')
+    renderItems(container,users)
+
+    const expectedContent = [
+      ' Name: John, Age: 30',
+      '(Admin) Name: Jane, Age: 25',
+      ' Name: Jack, Age: 40',
+    ];
+
+    renderItems(container, users);
+expect(Array.from(container.querySelectorAll('li')).map((li) => li.textContent)).toEqual(expectedContent);
+
+  })
+
+  test('should render proper content for given data with user permissions', ()=> {
+    localStorage.setItem('userRole', 'user'); // setting the user role to admin
+
+    const container = document.createElement('div')
+    renderItems(container,users)
+
+    const expectedContent = [
+      ' Name: John, Age: 30',
+      ' Name: Jack, Age: 40',
+    ];
+
+    renderItems(container, users);
+expect(Array.from(container.querySelectorAll('li')).map((li) => li.textContent)).toEqual(expectedContent);
+  
+    })
+  
 });
